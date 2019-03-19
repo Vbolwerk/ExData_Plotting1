ecc5c3087759c274308ee227b349d835d576b9c0
vbolwerk <vbolwerk@deloitte.nl>
2019-03-19 16:34:05 +0100

    Finalized plot 4 and added plots

--- plot4.R
+++ plot4.R
@@ -15,10 +15,18 @@ df$dateTime <- strptime(df$dateTime, format="%Y-%m-%d %H:%M:%S")
 
 
 ##Plot 4
-plot(y = df$Sub_metering_1, x = df$dateTime,col="Red", type = "l", ylab="Global Active Power (kilowatts)", xlab="")
-lines(y = df$Sub_metering_2, x = df$dateTime, col="Red")
-lines(y = df$Sub_metering_3, x = df$dateTime, col="Blue")
-legend("topright", lwd=c(1,1,1), col=c("black", "red", "blue"),  c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+par(mfrow=c(2,2))
+
+with(df, {
+  plot(y =Global_active_power, x= dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+  plot(y =Voltage, x= dateTime, type="l", ylab="Voltage (volt)", xlab="")
+  plot(y =Sub_metering_1, x= dateTime, col="Red", type="l", ylab="Global Active Power (kilowatts)", xlab="")
+  lines(y = Sub_metering_2, x = dateTime, col="Red")
+  lines(y = Sub_metering_3, x = dateTime, col="Blue")
+  legend("topright", lwd=c(1,1,1), col=c("black", "red", "blue"),  c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(y= Global_reactive_power, x=dateTime, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
+})
+
 
 
 dev.copy(png,"plot4.png", width=480, height=480)
